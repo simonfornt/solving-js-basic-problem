7f770c40874cf67b2c8ea160b40ada21060ebd95
simonfotnt <simonpiar98@gamil.com>
2025-01-30 00:14:07 +0600

    solved up to 14

---
+++ script.js
@@ -0,0 +1,229 @@
+// 1. Create a variable called carName, assign the value Volvo to it.
+
+// let carName = "Volvo";
+
+
+
+// 2. On one single line, declare three variables with the following names and values:
+      
+    /*
+        let firstName = "John"; let lastName = 'Doe'; let age = 35;  
+
+        let fullName = firstName + " " + lastName;
+
+        console.log(fullName);
+ */
+
+
+
+// 3. Use the correct assignment operator that will result in x being 50 (same as x = x * y).
+
+
+/*
+    let x = 10;
+    let y = 5;
+    x = x * y;
+ */
+
+
+
+//  4. Use comments to describe the correct data type of the following variables:
+
+/*
+let length = 16;  // number
+let lastNames = "Johnson"; // string
+
+const xx = {
+    firstName : "john",
+    lastName : "Doe"
+};          // object
+
+*/
+
+
+// 5. Execute the function named myFunction.
+
+/*
+     function myFunction(){
+     alert("Hello World");
+    }
+
+     myFunction();
+*/
+
+
+
+// 6. Create an object called person with name = John, age = 50, Then, access the object to alert("John is 50").
+
+/*
+    let person = {
+        name: "John",
+        age: 50,
+    }
+
+    alert(person.name + " is "+ person.age);
+
+ */   
+
+
+
+
+// 7.  The <button> element should do something when someone clicks on it. Try to fix it!
+
+
+/*
+        function howare(){
+            alert("How are you");
+        }
+
+        howare();
+
+*/
+
+
+
+
+// 8. The <button> element should do something when someone clicks on it. Try to fix it!
+// 8.1 Alert the number of items in an array, using the correct Array property: 
+
+/*
+    const cars = ["Volvo", "Jeep", "Mercedes"];
+    alert(cars.length);
+
+*/
+
+
+// 8.2 Change the first item of Brand to "Ford".
+
+/*
+	const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+    Brand[0] = "Ford"
+*/
+
+
+
+
+
+//  9.  Math Related Problems
+
+// 9.1. Use the correct Math method to create a random number.
+/*
+        const randomNum = Math.random();
+        console.log(randomNum);
+*/
+
+// 9.2. Use the correct Math method to return the largest number of 10 and 20.
+
+/*
+    const largestNum = Math.max(10, 20);
+*/
+
+
+// 9.3. Use the correct Math method to get the square root of 9.
+
+/*
+    const sNum = Math.sqrt(9)
+*/
+
+
+
+
+
+
+
+// 10 . comparison operator related problems! 
+
+// 10.1. Choose the correct comparison operator to alert true, when x is greater than y. 
+
+/*
+        x = 10;
+        y = 5;
+
+        if ( x > y){
+        alert(" x is bigger than y")
+        };
+*/
+
+// 10.2. Choose the correct conditional (ternary) operator to alert "Too young" if age is less than 18, otherwise alert "Old enough".
+
+/*
+    let age = 19 ;
+
+    if ( age < 18){
+        alert("Too Young")
+    } else{
+        alert("Old enough")
+    };
+
+*/
+
+
+
+
+
+// 11. Write a function to convert Celsius to Fahrenheit. The function should take a single argument, which is the temperature in Celsius.
+
+/*
+
+        function celsiusToFahrenheit(celsius) {
+        return ( celsius * 9/5) + 32 ;
+        }
+  
+        console.log(celsiusToFahrenheit(14)); // 32
+        console.log(celsiusToFahrenheit(25)); // 77
+*/
+
+
+
+
+// 12. Write a function to check if a number is even. The function should take a single argument, which is the number to check.
+
+/*
+
+        function isEven(num) {
+        return  num % 2 === 0;
+        }
+  
+        console.log(isEven(4)); // true
+        console.log(isEven(7)); // false
+  */
+
+
+
+
+
+
+// 13. Write a function to sum two numbers. The function should take two arguments, which are the numbers to sum.
+ 
+/*
+
+function sum(a, b) {
+    return a + b;
+  }
+  
+  console.log(sum(3, 4)); // 7
+  console.log(sum(10, 20)); // 30
+  
+*/
+
+
+
+
+// 14.  Write a function to find the smallest number in an array. The function should take a single argument, which is the array to search.
+
+/*
+
+        function findSmallestNum(arr) {
+        return Math.min(...arr);
+        }
+  
+        console.log(findSmallestNum([3, 5, 1, 9])); // 1
+        console.log(findSmallestNum([-1, -5, 0, 10])); // -5
+*/
+
+
+  
+
+
+
+ 
