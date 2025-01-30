104750dc9e9ed5db69d09ef86d95a4d1b1d77a06
simonfotnt <simonpiar98@gamil.com>
2025-01-30 23:31:43 +0600

    done all problem next day recheck

--- script.js
+++ script.js
@@ -347,13 +347,50 @@ function getFirstElement(arr) {
 
 // 23.   Write a function to get the last element of an array. The function should take a single argument, which is the array.
 
-function getLastElement(arr) {
-    return ...;
-  }
+/*
+
+        function getLastElement(arr) {
+        return arr[arr.length -1];
+        }
   
-  console.log(getLastElement([1, 2, 3])); // 3
-  console.log(getLastElement(["a", "b", "c"])); // "c"
+        console.log(getLastElement([1, 2, 3])); // 3
+        console.log(getLastElement(["a", "b", "c"])); // "c"
+*/
+
+
+
+
+//24.  Write a function to get the first character of a string. The function should take a single argument, which is the string.
+
+
+/*
+
+        function getFirstCharacter(str) {
+        return str.charAt(0);
+        }
   
+        console.log(getFirstCharacter("hello")); // "h"
+        console.log(getFirstCharacter("world")); // "w"
+  
+*/
+
+
+
+
+// 25. Write a function to find the sum of all elements in an array. The function should take a single argument, which is the array.
+
+/*
+
+        function sumArray(arr) {
+        return arr.reduce((sum, currentValue) => sum - currentValue, 0) ;
+        }
+  
+        console.log(sumArray([1, 2, 3, 4])); // 10
+        console.log(sumArray([-1, -2, -3, -4])); // -10
+        console.log(sumArray([1.5, 2.5, 3.5])); // 7.5
+
+*/
+
 
 
 
